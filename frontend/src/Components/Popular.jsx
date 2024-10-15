0ff1773824a0ad01631b73a1d2201cc11dedb533
Ranjit <shrestha.ranjit.np@gmail.com>
2024-10-15 13:43:18 +0545

    changes made

--- frontend/src/Components/Popular.jsx
+++ frontend/src/Components/Popular.jsx
@@ -5,9 +5,9 @@ const Popular = () => {
     <div className='padding'>
         <h1 className='titles'>Popular Recipes</h1>
         <section>
-            <div className='w-[16rem] h-[20rem] rounded-3xl shadow-lg shadow-slate-500'> 
-                <div></div>
-                <p>Title </p>
+            <div className='w-[20rem] rounded-3xl shadow-lg shadow-slate-500 grid justify-center p-3'> 
+                <div className='w-[16rem] h-[16rem] bg-slate-400'></div>
+                <p className='mt-3 mb-3'>Title </p>
                 <p>Genre</p>
             </div>
         </section>
