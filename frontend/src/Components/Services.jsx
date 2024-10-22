aba731ce9bbe26284bd586dbfa038307625e6bc6
Ranjit <shrestha.ranjit.np@gmail.com>
2024-10-22 12:00:16 +0545

    changes ade

--- frontend/src/Components/Services.jsx
+++ frontend/src/Components/Services.jsx
@@ -4,32 +4,37 @@ import Burger from '../assets/burger.png'
 const Services = () => {
   return (
     <div className='padding my-14 '>
-        <section className='w-full grid grid-cols-[20rem_1fr] h-64 rounded-3xl bg-customYellow relative items-center'>
-            <img src={Burger} className='absolute left-[-20px] w-[26rem] h-[160%]' />
-            <div className='flex items-center'>
-              <div className=''>
-                <div>
-                  <p>100</p>
-                  <p>More than 100 users active</p>
+        <section className=' w-full flex flex-row  items-center h-64 rounded-3xl bg-customYellow'>
+           
+             
+            
+              <div>
+              <img src={Burger} className='w-[28rem] h-[150%]' />
+
+              </div>
+  
+              <div className='flex gap-5 justify-center text-textColor bg-white p-6 pr-0 rounded-3xl' >
+                <div className='border-r-2 pr-6 border-customYellow text-center'>
+                  <p className='text-5xl font-extrabold mb-3'>100</p>
+                  <p>100+ users</p>
                 </div>
                 
-                <div>
-                  <p>1k</p>
-                  <p>Over a thousand recipe available</p>
+                <div className='border-r-2 pr-6 border-customYellow text-center'>
+                  <p className='text-5xl font-extrabold mb-3'>1k</p>
+                  <p>1K+ recipes</p>
                 </div>
 
-                <div>
-                  <p>Easy</p>
-                  <p>Easy recipes with detailed instructions and ingredients
+                <div className='pr-6 text-center'>
+                  <p className='text-5xl font-extrabold mb-3'>Easy</p>
+                  <p>Instructions and ingredients
                     
                   </p>
                 </div>
+                
 
 
 
               </div>
-
-            </div>
 
         </section>
     </div>
